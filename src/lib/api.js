d5f501473a5bb41f5f5af429c6c5978c4e4ec766
juber-nowawave <juber.nowawave@gmail.com>
2026-01-09 11:43:07 +0530

    ui changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
-const base_url = 'https://g79l1s80-5000.inc1.devtunnels.ms/api/news';
-// const base_url = 'http://localhost:5000/api/news';
+// const base_url = 'https://g79l1s80-5000.inc1.devtunnels.ms/api/news';
+const base_url = 'https://portal.stockezee.com/api/news';
 export const getNews = async () => {
   const response = await axios.get(base_url);
   return response.data;
